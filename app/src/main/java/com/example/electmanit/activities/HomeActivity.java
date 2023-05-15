4a3700ae0a8377e105cc1c3cdbcf321cab8f060f
Tanmay Sah <tanmay.sah14@gmail.com>
2023-05-15 22:32:39 +0530

    final commit before version 2.0

--- app/src/main/java/com/example/electmanit/activities/HomeActivity.java
+++ app/src/main/java/com/example/electmanit/activities/HomeActivity.java
@@ -1,8 +1,5 @@
 package com.example.electmanit.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.electmanit.R;
 import com.google.android.gms.tasks.OnCompleteListener;
